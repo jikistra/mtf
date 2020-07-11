c8c66ab4d9da0772a1ba41a5cde9aa4acb3db9e2
jik <kiyankaya@gmail.com>
2020-07-11 08:37:49 +0300

    degisiklikler yapıldı

--- app/Controllers/teklifformu.php
+++ app/Controllers/teklifformu.php
@@ -1,11 +1,11 @@
 <?php 
 namespace App\Controllers;
 class teklifformu extends BaseController{
-
+    
     public function index(){
-
-        echo "teklifformu";
-
+        
+       
+        return view('teklifformu');
     }
 
 
